c5ae5c6aa3a190bd0b51f3a8b599db5b6806650f
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-05 22:31:33 +0100

    created new route to a=manage portfolio

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require('path')
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const api_route = require("./routes/api_route")
+const api_route = require("./routes/api_route") 
+const portfolio_route = require("./routes/portfolio_route")
 const cors = require('cors')
 
 const port = process.env.PORT || 4000
@@ -15,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 
 app.use("/", api_route)
+app.use("/portfolio", portfolio_route)
 
 // mongoose.connect("mongodb://localhost/Tumblr_db", { useNewUrlParser: true, useUnifiedTopology: true }, 
 // err => { 
@@ -23,13 +25,13 @@ app.use("/", api_route)
 // }) 
 
 
-mongoose.connect("mongodb+srv://onojavoo:voo247@tumblr-db.fhyq2pm.mongodb.net/tumblr-blog?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, 
-err => {  
-    if (err) throw err 
-    console.log("Cloud Database connected")
-})
+// mongoose.connect("mongodb+srv://onojavoo:voo247@tumblr-db.fhyq2pm.mongodb.net/tumblr-blog?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, 
+// err => {  
+//     if (err) throw err 
+//     console.log("Cloud Database connected")
+// })
 
-mongoose.Promise=global.Promise
+// mongoose.Promise=global.Promise
 
 app.listen(4000, ()=>{
     console.log("Running on http://localhost:4000")
